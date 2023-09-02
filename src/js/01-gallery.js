a9cc3a0cb955e291cb8bd1659dbf6c2c8738bd3b
DamianKaczmarczyk <jokernick@o2.pl>
2023-09-02 20:00:32 +0200

    test2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,36 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+console.log(SimpleLightbox);
 console.log(galleryItems);
+
+const galleryList = document.querySelector('ul.gallery');
+console.log(galleryItems.length);
+
+let newElementGallery = '';
+
+for (let i = 0; i < galleryItems.length; i++) {
+  newElementGallery += `
+  <li>
+    <a class="gallery__link" href= ${galleryItems[i].original}>
+        <img
+            class="gallery__image"
+            src= ${galleryItems[i].preview}
+            alt=${galleryItems[i].description}
+            title="adpopoiuijng"
+        />
+    </a>
+  </li>
+`;
+}
+galleryList.insertAdjacentHTML('afterbegin', newElementGallery);
+
+const linka = document.querySelectorAll('.gallery__link');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = playTime => {
+  const { seconds } = playTime;
+  localStorage.setItem(TIME_KEY, seconds);
+  console.log(`Video current time is ${seconds} seconds.`);
+};
+
+const currentTime = localStorage.getItem(TIME_KEY);
+
+currentTime ? player.setCurrentTime(currentTime) : null;
+
+player.on('timeupdate', throttle(onPlay, 1000));
